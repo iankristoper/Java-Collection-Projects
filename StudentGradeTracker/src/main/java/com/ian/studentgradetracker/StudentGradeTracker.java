dbaed7d26719ec4ffc7257707bed15cca1fb8e6e
iankristoper <iankristoper26@gmail.com>
2024-09-05 22:56:09 +0800

    initial commit

--- StudentGradeTracker/src/main/java/com/ian/studentgradetracker/StudentGradeTracker.java
+++ StudentGradeTracker/src/main/java/com/ian/studentgradetracker/StudentGradeTracker.java
@@ -16,45 +16,132 @@ class StudentInformation
 {
     private String fullName;
     private int schoolID;
-    private float firstGrades;
-    private float secondGrades;
+    private float finalGrades;
     
     
     //constructor for data initialization
-    public StudentInformation(String fullName, int schoolID, float firstGrade, float secondGrade)
+    public StudentInformation(String fullName, int schoolID, float finalGrade)
     {
         this.fullName = fullName;
         this.schoolID = schoolID;
-        this.firstGrades = firstGrade;
-        this.secondGrades = secondGrade;
+        this.finalGrades = finalGrade;
     }
     
     
-    //public getters
-    /*
-    *
-    *
-    *
-    *
-    */      
+    //public setters for updating or setting new data to private data
+    public void setName(String name) {
+        this.fullName = name;
+    }
+    
+    public void setID(int id) {
+        this.schoolID = id;
+    }
+    
+    public void setGrades(float grades) {
+        this.finalGrades = grades;
+    }    
+    
+    
+    //public getters for getting the private data 
+    public String getName() {
+        return fullName;
+    }
+    
+    public int getID() {
+        return schoolID;
+    }
+    
+    public float getGrades() {
+        return finalGrades;
+    }
+      
+    
+    //method overriding -> override the toString() method
+    @Override
+    public String toString()
+    {
+        return "God is good";
+    }
+    
+          
 }
 
 
 
 class GradesTracker
 {
-    Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
     private static List<StudentInformation> studentList = new ArrayList<>();  //use the list interface and the arraylist as the implementation
     
-    int id = 1000;
+    static int id = 1000;
     
+    
+    
+    //this method is for adding student information 
     public static void addStudent()
     {
-        System.out.print("Enter your full name [First name, Last Name]: ");
+        System.out.print("Enter student full name [First name, Last Name]: ");
         String studentName = scanner.nextLine();
         
-        System.out.print("");
+        System.out.println("Student ID number: " + id++);
+        int studentID = id;
+        
+        System.out.print("Enter student final grade: ");
+        float studentGrade = scanner.nextFloat();
+        
+        
+        StudentInformation studentInformation = new StudentInformation(studentName, studentID, studentGrade);
+        studentList.add(studentInformation);
+        
+        System.out.println("Student information is added successfully");
     }
+    
+    
+    
+    //this method is for updating the student records
+    public static void updateGrades()
+    {
+        System.out.print("Enter student ID: ");
+        int searchID = scanner.nextInt();
+        
+        for(StudentInformation copy : studentList)
+        {
+            if(copy.getID() == searchID)
+            {
+                System.out.print("Enter ");
+                return;
+            }
+        }
+        
+        System.out.println("Invalid ID");         
+            
+    }
+    
+    
+    
+    //this method is for viewing the all available grades on the memory
+    public static void viewGrades()
+    {
+        System.out.println("Here are the list of students and their grades");
+        System.out.println("");
+        
+        for(StudentInformation copy : studentList)
+        {
+            System.out.println(copy);
+            System.out.println("");
+        }
+        
+        System.out.println("================================");
+    }
+    
+    
+    
+    public static void searchStudent()
+    {
+        
+    }
+    
+    
         
         
 }
