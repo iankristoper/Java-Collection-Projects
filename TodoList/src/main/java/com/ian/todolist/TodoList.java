fd52f83cfaeb6ef54bbc6c04d6bf2ade991571c6
iankristoper <iankristoper26@gmail.com>
2024-09-06 23:28:06 +0800

    initial commit

---
+++ TodoList/src/main/java/com/ian/todolist/TodoList.java
@@ -0,0 +1,32 @@
+package com.ian.todolist;
+
+
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+
+
+
+
+
+
+
+
+
+
+
+public class TodoList 
+{
+
+    public static void main(String[] args) 
+    {
+        List<String> firstPart = new ArrayList<>();
+        List<String> secondPart = new ArrayList<>();
+        
+        
+       
+        System.out.println(firstPart);
+        
+    }
+}
