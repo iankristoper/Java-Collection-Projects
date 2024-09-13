ea454fa10bf9e5cdaf9990de0e8a46b1ebd55269
iankristoper <iankristoper26@gmail.com>
2024-09-13 21:18:15 +0800

    initial commit

---
+++ BookCollection/src/main/java/com/ian/bookcollection/BookCollection.java
@@ -0,0 +1,33 @@
+package com.ian.bookcollection;
+
+
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+
+
+//
+class Book
+{
+    private String bookID;
+    private String bookName;
+    private String bookAuthor;
+    private float bookRating;
+    
+    
+    
+}
+
+
+
+
+
+
+public class BookCollection 
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+}
