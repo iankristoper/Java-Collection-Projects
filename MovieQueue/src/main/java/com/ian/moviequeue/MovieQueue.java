e2317b14a07d1159495668d9e9290770a90b0ca6
iankristoper <iankristoper26@gmail.com>
2024-09-17 23:31:17 +0800

    initial commit

---
+++ MovieQueue/src/main/java/com/ian/moviequeue/MovieQueue.java
@@ -0,0 +1,36 @@
+package com.ian.moviequeue;
+
+
+
+import java.util.Scanner;
+import java.util.List;
+import java.util.LinkedList;
+
+
+
+
+//
+class Accounts
+{
+    private int id;
+    private String name;
+    private String password;
+    
+    
+}
+
+
+class Movie
+{
+    
+}
+
+
+public class MovieQueue 
+{
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World!");
+    }
+}
---
+++ UniqueEmailValidator/src/main/java/com/ian/uniqueemailvalidator/UniqueEmailValidator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ian.uniqueemailvalidator;
+
+/**
+ *
+ * @author Ian Tagano
+ */
+public class UniqueEmailValidator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
