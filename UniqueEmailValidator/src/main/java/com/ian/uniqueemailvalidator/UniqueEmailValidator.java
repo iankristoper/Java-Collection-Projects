8f867c1713a92d84ffa87b9387f71eef44298960
iankristoper <iankristoper26@gmail.com>
2024-09-19 22:37:33 +0800

    minor changes

--- UniqueEmailValidator/src/main/java/com/ian/uniqueemailvalidator/UniqueEmailValidator.java
+++ UniqueEmailValidator/src/main/java/com/ian/uniqueemailvalidator/UniqueEmailValidator.java
@@ -13,20 +13,50 @@ import java.util.HashSet;
 
 public class UniqueEmailValidator 
 {
-    public static void main(String[] args) 
+    public static void main(String[] args)
     {      
-        Set<String> sampleSet = new HashSet<String>();
-        
-        sampleSet.add("ian");
-        sampleSet.add("rhina");
+        Scanner scanner = new Scanner(System.in);
+        boolean isMainConditionRunning = true;
         
         
-        for(String copy : sampleSet)
-        {
+        while(isMainConditionRunning) {
+            System.out.println("");
+            System.out.println("[1] Add email");
+            System.out.println("[2] Check email");
+            System.out.println("[3] Remove email");
+            System.out.println("[4] View all emails");
+            System.out.println("[5] Remove all emails");
+            System.out.println("");
+            
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();  //consume the newline
+            
+            
+            switch(choice) {
+                case 1:
+                    break;
+                    
+                case 2:
+                    break;
+                    
+                case 3:
+                    break;
+                    
+                case 4: 
+                    break;
+                    
+                case 5:
+                    break;
+                    
+                case 6:
+                    isMainConditionRunning = false;
+                    break;
+            }
+            
             
         }
-        
-        
+            
         
         
     }
