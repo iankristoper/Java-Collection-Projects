3daec3d8ab6a71f0fdbf6fa48666269963a25dcd
iankristoper <iankristoper26@gmail.com>
2024-09-20 21:13:02 +0800

    initial changes

--- UniqueEmailValidator/src/main/java/com/ian/uniqueemailvalidator/UniqueEmailValidator.java
+++ UniqueEmailValidator/src/main/java/com/ian/uniqueemailvalidator/UniqueEmailValidator.java
@@ -8,12 +8,140 @@ import java.util.HashSet;
 
 
 
+/*
+ * This class is for the data, this promotes encapsulation
+ */
+
+class Email
+{
+    private String email;
+    
+    
+    //constructor for this class
+    public Email(String email) {
+        this.email = email;
+    }
+    
+    
+    //getters and setters
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    //method overriding
+               
+}
 
 
 
+
+/*
+ * This class is the logic of the features of the system
+ */
+
+class EmailFunction
+{
+    private Set<Email> set = new HashSet<Email>();
+    
+    
+    
+    //this method is for adding the email logic 
+    public void addingEmail(String email) {
+        Email mail = new Email(email); //create an email object then add the value to the constructor
+        set.add(mail); //add mail object to the set
+    }
+    
+    
+    //this method is for checking email logic
+    public void checkingEmail(String email) {
+        //traverse the set to check if there is existing email
+        for(Email copy : set) {
+            if(copy.getEmail() == email) {
+                System.out.println("Email exist!");
+                return;
+            }         
+        }
+        
+        System.out.println("Email doesn't exist");
+                     
+    }
+    
+    
+    //this method is for checking if the hashtable is empty
+    public void checkHashTable() {
+        if(set.isEmpty()) {
+            System.out.println("No email inside the storage");
+            return;
+        }
+        
+    }
+    
+    
+    
+}
+
+
+
+
+/*  
+ * This class is the features of the system
+ */
+
+class EmailServices
+{
+    private Scanner scanner = new Scanner(System.in);
+    EmailFunction function = new EmailFunction();
+    
+    
+    //this method is for adding the email 
+    public void addEmail() {
+        System.out.print("Enter email: ");
+        String addMail = scanner.nextLine();
+        
+        function.addingEmail(addMail);  //use the logic method
+        
+        System.out.println("Email added successfully!");      
+        
+    }
+    
+    
+    //this method is for checking email if it exist in the hash table
+    public void checkEmail() {
+        System.out.print("Enter email to check: ");
+        String checkMail = scanner.nextLine();
+        
+        function.checkingEmail(checkMail);  //use the logic method
+        
+    }
+    
+    
+    //this method is removing existing email
+    public void removeEmail() {
+        function.checkHashTable();  //this method check if there is any email in the hashtable
+        
+        System.out.print("Enter email to remove: ");
+        String removeMail = scanner.nextLine();
+        
+        =================================================================================================
+    }
+    
+    
+    
+    
+}
+
+
+
+
+//main class 
 public class UniqueEmailValidator 
 {
-    public static void main(String[] args)
+    public static void main(String[] args) 
     {      
         Scanner scanner = new Scanner(System.in);
         boolean isMainConditionRunning = true;
